0a1376c33ebb6e86b56c17359f29a06001dd44c8
A . Chethan chandu <chethanchandu8247@gmail.com>
2026-05-11 10:41:33 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,38 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import Header from './components/Header'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import CountryPage from './pages/CountryPage'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route
+            path="/country/:code"
+            element={<CountryPage />}
+          />
+
+          <Route
+            path="/favourites"
+            element={<div>Favourites Page</div>}
+          />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default App
